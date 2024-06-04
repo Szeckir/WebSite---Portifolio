f1e9d9e7cd9be756af0307197dd25c5cf5ec700f
Szeckir <80286620+Szeckir@users.noreply.github.com>
2024-06-04 17:09:26 -0300

    :construction: site formatado, adicao de animacoes e novas atualizacoes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,9 +5,15 @@ btnEmoji.innerHTML = '😎'
 function mudarEmojiMenuPrincipal() {
     const emojis = ['😊', '😎', '😍', '🥳', '😂', '😜', '😇', '🤩', '😁', '🤔'];
     const randomPosition = Math.floor(Math.random() * emojis.length);
-    
+
     btnEmoji.innerHTML = emojis[randomPosition]
 }
+
+// ARRUMAR AQUI A MUDANCA DE IMAGEM QUANDO CLICA NO EMOJI
+// function mudaImagemMenuPrincipal() {
+//     const imgMenu = document.querySelector("side-bar-thomaz-img")
+//     imgMenu.innerHTML += '<img src="/public/images/img-logo.png" alt="Imagem de Thomaz Gomes Szeckir">' 
+// }
 
 const fundo = document.querySelector('#body')
 const btnMudarFundo = document.querySelector('#btn-change-theme')
@@ -21,14 +27,64 @@ function mudarCorDoFundo() {
         fundo.classList.add("body")
         btnMudarFundo.innerHTML = 'Light-Mode'
         btnMudarFundo.classList.add("btn-dark-mode")
+        if (btnMudarFundo.classList.contains("btn-light-mode")) {
+            btnMudarFundo.classList.remove("btn-light-mode")
+        }
         variable = true;
         return
     } else {
         fundo.classList.remove("body")
         btnMudarFundo.innerHTML = 'Dark-Mode'
+        if (btnMudarFundo.classList.contains("btn-dark-mode")) {
+            btnMudarFundo.classList.remove("btn-dark-mode")
+        }
         btnMudarFundo.classList.add("btn-light-mode")
         variable = false
         return
     }
+}
 
+document.querySelector("#project-item-acerte-numero").addEventListener('onclick', abrirProjetoAcerteNumero)
+var projectOpen = false;
+
+function abrirProjetoAcerteNumero() {
+    const btnClass = document.querySelector("#project-item-acerte-numero")
+    const textClass = document.querySelector("#project-item-open-text")
+
+    if(projectOpen == false) {
+        btnClass.classList.remove("project-item")
+        btnClass.classList.add("project-item-after-click")
+        projectOpen = true;
+        adicionarTextoAcerteNumero()
+    } else {
+        btnClass.classList.remove("project-item-after-click")
+        btnClass.classList.add("project-item")
+        textClass.innerHTML = ''
+        projectOpen = false
+    }
+}
+
+function adicionarTextoAcerteNumero() {
+    const newText = document.querySelector("#project-item-open-text")
+
+    newText.innerHTML += `
+    <div class="project-item-text">
+        <br>
+        <p> Este jogo foi desenvolvido como forma de treinar e praticar meus conhecimentos de JavaScript, utilizando uma API de reconhecimento de fala. </p>        
+        <br>
+        <p> Este jogo também inclui noções básicas de HTML e CSS </p> 
+        <br>
+        <p> O jogo é muito intuitivo e fácil de jogar. </p>
+    </div>
+
+        <div class="project-images" id="project-img-acerte-numero">
+            <img src="/public/images/acerte-numero-menu.png" alt="Imagem Jogo">
+            <img src="/public/images/acerte-numero-playing.png" alt="Imagem Jogo">
+        </div>
+    `
+}
+
+
+function addTextoExperiencia() {
+    
 }
